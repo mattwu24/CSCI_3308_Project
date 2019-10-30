b574607b96f5b803f2176981e324e0a635a3d2a4
Erik Pohle <erikpohle@hotmail.com>
2019-10-29 17:28:00 -0700

    Added JavaScript to get google maps location finding working

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,6 @@
-var userLoc = "";
-
 $(document).ready(function(){
 
-    userLoc = $('#userLoc');
+    var userLoc = $('#userLoc');
     var submit = $("#submitLoc");
     submit.click(function() {
         if(userLoc.val() == undefined || userLoc.val() === '') {
@@ -14,19 +12,61 @@ $(document).ready(function(){
   
   });
 
-function getLocation() {
-    if (navigator.geolocation) {
-        userAllow = true;
-        navigator.geolocation.getCurrentPosition(showPosition, function(position) {
-            alert('Location Permissions Denined.');
-        });
-    } 
-}
-    
-function showPosition(position) {
-    var locOut = document.getElementById("locOut");
-    locOut.innerHTML="Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
+var geocoder;
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(40.0150, -105.2705);
+  var mapOptions = {
+    zoom: 12,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 
-  
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function codeAddressIP() {
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
